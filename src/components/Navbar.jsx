a8bda67ef1dba7aa3635431d72aa420b0881487c
Diti Arora <diti.aromatic@gmail.com>
2024-03-20 23:50:08 +0530

    website done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,10 +6,10 @@ export default function Navbar() {
     <div className="flex justify-between items-center font-sans shadow-md sticky top-0 bg-white">
         <a href="/"><img className="h-14 pl-4" src={ss.src} alt="kya itna bura hu mai maaa" /></a>
         <div className="flex items-center text-content gap-10 text-gray-500 pr-12">
-          <a className="text-black py-4 " href="">Home</a>
-          <a className="hover:text-black font-[500] transition duration-300 ease-in-out py-5 " href="">Linkedln</a>
-          <a className="hover:text-black font-[500] transition duration-300 ease-in-out py-5 " href="">Twitter</a>
-          <a className="hover:text-black font-[500] transition duration-300 ease-in-out py-5 " href="">Dribbble</a>
+          <a className="text-black py-4 " href="/">Home</a>
+          <a className="hover:text-black font-[500] transition duration-300 ease-in-out py-5 " href="https://www.linkedin.com/in/tejasviarora">Linkedln</a>
+          <a className="hover:text-black font-[500] transition duration-300 ease-in-out py-5 " href="https://twitter.com/TejasviArora17">Twitter</a>
+          <a className="hover:text-black font-[500] transition duration-300 ease-in-out py-5 " href="">Resume</a>
         </div>
     </div>
   )
